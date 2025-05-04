c9a140966b0f7597f2da4e0c849d7a66e502929f
Daniel Leussa <67153034+danofred00@users.noreply.github.com>
2025-05-04 17:48:12 +0100

    Initial commit

---
+++ backend/src/main/java/cm/group/gestion_laverie/GestionLaverieApplication.java
@@ -0,0 +1,13 @@
+package cm.group.gestion_laverie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionLaverieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionLaverieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/cm/group/gestion_laverie/GestionLaverieApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.group.gestion_laverie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionLaverieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
